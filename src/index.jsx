9210b1ad114fca06977df23fe4038aa99461c246
Caio Duarte <caiosduarte@yahoo.com.br>
2019-10-29 12:35:14 -0300

    primeira versao com o webpack

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import './icon.png'
+import './css/reset.css'
+import './css/main.css'
+
+ReactDOM.render(
+    document.querySelector("#header"),
+    document.querySelector("#main"),
+    document.querySelector("#footer")    
+)
+
+
+import './js/vendor/jquery-3.4.1.min.js'
+import './js/ponto.js'
+import './js/mapa.js'
+
+
