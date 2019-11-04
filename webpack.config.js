42da63e68cc228794cbed5b9513d9e7a21ed1fa6
Caio Duarte <caiosduarte@yahoo.com.br>
2019-11-04 00:46:35 -0200

    descomentado o uglify

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src/index.jsx'),
@@ -53,7 +53,7 @@ module.exports = {
             template: path.join(__dirname, 'src/index.html')
         }),
         new ExtractTextPlugin('style.css')//,
-        //new UglifyJSPlugin()
+        new UglifyJSPlugin()
     ],
     devServer: {
         publicPath: "/",
