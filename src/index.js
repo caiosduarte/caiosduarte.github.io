bd9fb55cb14db6b0eeb3f9e5749c681701244017
Caio's Zenbook <caiosduarte@gmail.com>
2019-12-08 19:02:46 -0300

    ajustes no webpack arquivo index.js e no registro do sw.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './icon.png'
+import './favicon.ico'
+import './css/reset.css'
+import './css/main.css'
+import './js/main.js'
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+        console.log('service worker registrado');
+    })
+    .catch(function (err) {
+        console.warn('service worker não registrado. erro:', err);
+    });
+}
+
+
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, 'src/index.jsx'),
+    entry: path.join(__dirname, 'src/index.js'),
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
